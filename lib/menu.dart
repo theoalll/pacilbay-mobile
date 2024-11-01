8645eb6eddb6f16fb441678341d7849d581a6e37
theoalll <theo.ananda@ui.ac.id>
2024-11-01 13:24:04 +0700

    make info card

--- lib/menu.dart
+++ lib/menu.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
 
+  final String npm = '2306165660';
+  final String name = 'Theo Ananda Lemuel';
+  final String className = 'PBP A';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,3 +14,36 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class InfoCard extends StatelessWidget {
+  // Kartu informasi yang menampilkan title dan content.
+
+  final String title;  // Judul kartu.
+  final String content;  // Isi kartu.
+
+  const InfoCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // Membuat kotak kartu dengan bayangan dibawahnya.
+      elevation: 2.0,
+
+      child: Container(
+        // Mengatur ukuran dan jarak di dalam kartu.
+        width: MediaQuery.of(context).size.width / 3.5, // menyesuaikan dengan lebar device yang digunakan.
+        padding: const EdgeInsets.all(16.0),
+        // Menyusun title dan content secara vertikal.
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+}
